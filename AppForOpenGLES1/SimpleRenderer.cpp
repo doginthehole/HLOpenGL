2e7ba860168f83ee84b2d8aa8036a608407f4c00
Tom Crane <tcrane12@gmail.com>
2016-09-29 15:51:46 -0400

    Still working on shader s***

--- AppForOpenGLES1/SimpleRenderer.cpp
+++ AppForOpenGLES1/SimpleRenderer.cpp
@@ -6,7 +6,6 @@
 #include "SimpleRenderer.h"
 #include "MathHelper.h"
 #include "vtkPolyDataNormals.h"
-#include <shader.hpp>
 #include <vector>
 #include <iostream>
 #include <fstream>
@@ -16,6 +15,7 @@
 #include <iostream>
 #include <fstream>
 #include "PolyDataReceiver.cpp"
+#include "esUtil.h"
 
 using namespace std;
 using namespace Platform;
@@ -281,45 +281,9 @@ SimpleRenderer::SimpleRenderer(bool isHolographic) :
 
 
 	//Let's just have some space to setup the lighting for this module
-	/*
-	GLuint LoadShaders(GLenum type, const char *shaderSrc) 
-	{
-		GLuint shader;
-		GLint compiled;
-
-		//create the shader object
-		shader = glCreateShader(type);
-
-		if (shader == 0)
-			return 0;
 	
-		// load shader source
-		glShaderSource(shader, 1, &shaderSrc, NULL);
+	
 
-		//compile the shader
-		glCompileShader(shader);
-
-		//Check compilation
-		glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
-
-		if (!compiled)
-		{
-			GLint inforLen = 0;
-
-			glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
-
-			if (infoLen > 1)
-			{
-				char* infoLog = malloc(sizeof(char) * infoLen);
-
-				glGetShaderInfoLog(shader, infoLen, NULL, infoLog);
-				esLogMessage("error compiling shader:\n%s\n", infoLog);
-
-				free(infoLog);
-			}
-		}
-	}
-*/
 
 	//The surface normals will need to be calculated 
 
@@ -381,6 +345,9 @@ SimpleRenderer::SimpleRenderer(bool isHolographic) :
 		(void*)0                          // array buffer offset
 	);
 
+
+	//let's load those shaders
+
 	mIsHolographic = isHolographic;
 }
 
@@ -411,6 +378,8 @@ SimpleRenderer::~SimpleRenderer()
 	}
 }
 
+
+
 template < class X >             // define template function
 X clamp(X input, X min, X max)
 {
@@ -422,6 +391,136 @@ X clamp(X input, X min, X max)
 	return input;
 
 	return input;
+}
+
+
+
+///from hello triangle
+GLuint LoadShader(GLenum type, const char *shaderSrc)
+{
+	GLuint shader;
+	GLint compiled;
+
+	// Create the shader object
+	shader = glCreateShader(type);
+
+	if (shader == 0)
+	{
+		return 0;
+	}
+
+	// Load the shader source
+	glShaderSource(shader, 1, &shaderSrc, NULL);
+
+	// Compile the shader
+	glCompileShader(shader);
+
+	// Check the compile status
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+
+	if (!compiled)
+	{
+		GLint infoLen = 0;
+
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
+
+		if (infoLen > 1)
+		{
+			//infoLen = (char *infoLen)  malloc(sizeof(char));
+
+			//char* infoLog = new char(infoLen);
+
+			GLchar * infoLog = nullptr;
+
+			glGetShaderInfoLog(shader, infoLen, NULL, infoLog);
+			//esLogMessage("Error compiling shader:\n%s\n", infoLog);
+
+			free(infoLog);
+		}
+
+		glDeleteShader(shader);
+		return 0;
+	}
+
+	return shader;
+
+}
+
+
+///from hello triangle
+// Initialize the shader and program object
+int Init(ESContext *esContext)
+{
+	//UserData *userData = esContext->userData;
+	//UserData *userData = esContext->userData;
+	char vShaderStr[] =
+		"#version 300 es                          \n"
+		"layout(location = 0) in vec4 vPosition;  \n"
+		"void main()                              \n"
+		"{                                        \n"
+		"   gl_Position = vPosition;              \n"
+		"}                                        \n";
+
+	char fShaderStr[] =
+		"#version 300 es                              \n"
+		"precision mediump float;                     \n"
+		"out vec4 fragColor;                          \n"
+		"void main()                                  \n"
+		"{                                            \n"
+		"   fragColor = vec4 ( 1.0, 0.0, 0.0, 1.0 );  \n"
+		"}                                            \n";
+
+	GLuint vertexShader;
+	GLuint fragmentShader;
+	GLuint programObject;
+	GLint linked;
+
+	// Load the vertex/fragment shaders
+	vertexShader = LoadShader(GL_VERTEX_SHADER, vShaderStr);
+	fragmentShader = LoadShader(GL_FRAGMENT_SHADER, fShaderStr);
+
+	// Create the program object
+	programObject = glCreateProgram();
+
+	if (programObject == 0)
+	{
+		return 0;
+	}
+
+	glAttachShader(programObject, vertexShader);
+	glAttachShader(programObject, fragmentShader);
+
+	// Link the program
+	glLinkProgram(programObject);
+
+	// Check the link status
+	glGetProgramiv(programObject, GL_LINK_STATUS, &linked);
+
+	if (!linked)
+	{
+		GLint infoLen = 0;
+
+		glGetProgramiv(programObject, GL_INFO_LOG_LENGTH, &infoLen);
+
+		/*if (infoLen > 1)
+		{
+			char *infoLog = malloc(sizeof(char) * infoLen);
+
+			glGetProgramInfoLog(programObject, infoLen, NULL, infoLog);
+			esLogMessage("Error linking program:\n%s\n", infoLog);
+
+			free(infoLog);
+		}*/
+
+		glDeleteProgram(programObject);
+		return FALSE;
+	}
+
+	// Store the program object
+	//userData->programObject = programObject;
+
+	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+	return TRUE;
 }
 
 
--- AppForOpenGLES1/polydatareceiver.cpp
+++ AppForOpenGLES1/polydatareceiver.cpp
@@ -7,7 +7,6 @@
 //
 
 #include <stdio.h>
-
 #include <iostream>
 #include <iomanip>
 #include <math.h>
